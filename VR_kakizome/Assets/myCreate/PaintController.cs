24d4f11cd1ff5d30e9d2675c32f9576dd3ce81ca
odrum428 <oxkeita@gmail.com>
2020-01-06 03:40:47 +0900

    ハンドトラッキングを実装

--- VR_kakizome/Assets/myCreate/PaintController.cs
+++ VR_kakizome/Assets/myCreate/PaintController.cs
@@ -1,35 +1,58 @@
-﻿using System.Collections;
 using UnityEngine;
+using System.Collections;
 
-public class PaintController : MonoBehaviour {
+public class PaintController : MonoBehaviour
+ {
 
-	Texture2D drawTexture;
+	[SerializeField] Transform HandAnchor;//positionを取得するコントローラーの位置情報
+	
+	// コントローラーを設定
+	[SerializeField] OVRInput.Controller controller;
+
+	private Transform Pointer
+	{
+	    get
+	    {
+		    return HandAnchor;
+	    }
+	}
+
+	Texture2D drawTexture ;
 	Color[] buffer;
 
-	void Start() {
-		Texture2D mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
+	void Start () {
+        	// OVRControllerHelperから左右どっちかを取得する
+	      controller = GetComponent<OVRControllerHelper>().m_controller;
+
+		Texture2D mainTexture = (Texture2D) GetComponent<Renderer>().material.mainTexture;
 		Color[] pixels = mainTexture.GetPixels();
 
 		buffer = new Color[pixels.Length];
 		pixels.CopyTo(buffer, 0);
 
-		// 画面上半分を塗りつぶす
-		for(int x = 0; x < mainTexture.width; x++){
-			for(int y = 0; y < mainTexture.height; y++){
-				if( y < mainTexture.height/2 ){
-					buffer.SetValue(Color.black, x + 256 * y);
-				}
-			}
-		}
-
 		drawTexture = new Texture2D(mainTexture.width, mainTexture.height, TextureFormat.RGBA32, false);
 		drawTexture.filterMode = FilterMode.Point;
 	}
 
+	public void Draw(Vector2 p)
+	{
+		buffer.SetValue(Color.black, (int)p.x + 256 * (int)p.y);
+	}
+
 	void Update() 
 	{
-		drawTexture.SetPixels(buffer);
-		drawTexture.Apply();
-		GetComponent<Renderer>().material.mainTexture = drawTexture;
+		var pointer = Pointer;
+		if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger, controller)) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay (pointer.position);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100.0f)) {
+				Draw (hit.textureCoord * 256);
+			}
+
+			drawTexture.SetPixels (buffer);
+			drawTexture.Apply ();
+			GetComponent<Renderer> ().material.mainTexture = drawTexture;
+		}
 	}
 }
