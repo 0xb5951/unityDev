ff68e67d902be09c7b11273dac0d6cf13e2ca681
odrum428 <oxkeita@gmail.com>
2020-01-06 02:11:38 +0900

    yousiに色が付与されることを確認

--- VR_kakizome/Assets/myCreate/PaintController.cs
+++ VR_kakizome/Assets/myCreate/PaintController.cs
@@ -1,36 +1,35 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PaintController : MonoBehaviour {
 
-	Texture2D drawTexture ;
+	Texture2D drawTexture;
 	Color[] buffer;
 
-	void Start () {
-		Texture2D mainTexture = (Texture2D) GetComponent<Renderer> ().material.mainTexture;
+	void Start() {
+		Texture2D mainTexture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
 		Color[] pixels = mainTexture.GetPixels();
 
 		buffer = new Color[pixels.Length];
-		pixels.CopyTo (buffer, 0);
+		pixels.CopyTo(buffer, 0);
 
 		// 画面上半分を塗りつぶす
 		for(int x = 0; x < mainTexture.width; x++){
 			for(int y = 0; y < mainTexture.height; y++){
-				if( y < mainTexture.height / 2 ){
-					buffer.SetValue (Color.black, x + 256 * y);
+				if( y < mainTexture.height/2 ){
+					buffer.SetValue(Color.black, x + 256 * y);
 				}
 			}
 		}
 
-		drawTexture = new Texture2D (mainTexture.width, mainTexture.height, TextureFormat.RGBA32, false);
+		drawTexture = new Texture2D(mainTexture.width, mainTexture.height, TextureFormat.RGBA32, false);
 		drawTexture.filterMode = FilterMode.Point;
 	}
 
-	void Update () 
+	void Update() 
 	{
-		drawTexture.SetPixels (buffer);
+		drawTexture.SetPixels(buffer);
 		drawTexture.Apply();
-		GetComponent<Renderer> ().material.mainTexture = drawTexture;
+		GetComponent<Renderer>().material.mainTexture = drawTexture;
 	}
 }
